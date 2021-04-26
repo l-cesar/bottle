9aa09c69511504c870c540c701595747f6601db2
asari <32879431+deomorxsy@users.noreply.github.com>
2021-04-26 03:06:47 -0300

    creating feed

--- headline.py
+++ headline.py
@@ -9,8 +9,17 @@ bbc_feed = "http://feeds.bbci.co.uk/news/rss.xml"
 @app.route('/')
 def get_news():
     feed = feedparser.parse(bbc_feed)
-    first_article = feed['entries']
-    return 'lorem ipsum dolor sit amet'
+    first_article = feed['entries'][0]
+    return '''
+        <html>
+            <body>
+                <h1> BBC Headlines </h1>
+                <b>{0}</b> <br/>
+                <i>{1}</i> <br/>
+                <p>{2}</p> <br/>
+            </body>
+        </html>'''.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
 
 @app.after_request
 def add_header(response):
