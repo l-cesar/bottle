9e071426bd99507940c0ab1daddb21a07b2697ed
asari <32879431+deomorxsy@users.noreply.github.com>
2021-04-26 12:16:31 -0300

    removing __pycache__

--- headline.py
+++ headline.py
@@ -14,9 +14,9 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route('/')
 @app.route('/<publication>')
 def get_news(publication='bbc'):
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-	return render_template("home.html", title=first_article.get("title"), published=first_article.get("published"), summary=first_article.get("summary"))
+	feed = feedparser.parse(RSS_FEEDS[publication])
+	first_article = feed['entries'][0]
+	render_template("home.html", title=first_article.get("title"), published=first_article.get("published"), summary=first_article.get("summary"))
 
 @app.after_request
 def add_header(response):
